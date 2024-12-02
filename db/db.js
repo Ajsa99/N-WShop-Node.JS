049dc00af49595ee8cc2bb52edad08ae1f078a94
Ajsa99 <ajsa.alibasic11@gmail.com>
2024-12-02 14:26:29 +0100

    Add

--- db/db.js
+++ db/db.js
@@ -14,21 +14,16 @@
 
 const mysql = require('mysql2')
 require("dotenv").config();
-
+  
 const db = mysql.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-  });
-
-  //      DB_HOST: 'sql8.freesqldatabase.com', 
-  // DB_USERNAME: 'sql8748198', 
-  // DB_PASSWORD: 'dAuGMhsAYf',
-  // DB_DBNAME: 'sql8748198',
+  host: 'sql8.freesqldatabase.com', 
+  user: 'sql8748198', 
+  password: 'dAuGMhsAYf',
+  database: 'sql8748198',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
   
   module.exports = db;
   
