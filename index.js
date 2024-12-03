838bcc162b9c78f989657f99ba6706b031ece0cd
Ajsa99 <ajsa.alibasic11@gmail.com>
2024-12-03 14:05:55 +0100

    change

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-// const express = require('express');
-// const cors = require("cors");
-// require("dotenv").config();
+const express = require('express');
+const cors = require("cors");
+require("dotenv").config();
 
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-
-// const Port = 5000;
-
-// const RegLog = require("./Router/RegLog/reg_log");
-// const User = require("./Router/Users/User");
-// const Product = require("./Router/Product/product");
-
-// app.use("/", User);
-// app.use("/", RegLog);
-// app.use("/", Product);
-
-// app.listen(process.env.PORT || Port, () => {
-//   console.log(`Service running on port ${Port}`);
-// });
-
-const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+const Port = 5000;
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const RegLog = require("./Router/RegLog/reg_log");
+const User = require("./Router/Users/User");
+const Product = require("./Router/Product/product");
+
+app.use("/", User);
+app.use("/", RegLog);
+app.use("/", Product);
+
+app.listen(process.env.PORT || Port, () => {
+  console.log(`Service running on port ${Port}`);
 });
